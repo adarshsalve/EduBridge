302f374738e67c66e254f7473ca5f68a3138601a
adarshsalve <salve.adarsh@kgce.edu.in>
2023-11-08 15:43:14 +0530

    python file add to git hub repo

---
+++ arithmaticcollege.py
@@ -0,0 +1,11 @@
+a=3
+b=4
+print("the sum of a and b :",a+b)
+print("the Division of a and b :",a/b)
+print("the Multiplication of a and b :",a*b)
+print("the Difference of a and b :",a+b)
+bool1=True
+bool2=False
+print("the value of bool1 and bool2 is ",(bool1 and bool2))
+print("the value of bool1 and bool2 is ",(bool1 or bool2))
+print("the value of bool1 and bool2 is ",(not bool2))
